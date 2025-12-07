e397b11a4f6f0274cd9d3a434c958b307b1fa195
Mobo4 <31019244+Mobo4@users.noreply.github.com>
2025-12-07 08:57:56 -0800

    Implement Real Tracking: GTM-NS6QTKN, GA4 & Ads Tags

--- components/GoogleTag.tsx
+++ components/GoogleTag.tsx
@@ -5,21 +5,42 @@ import Script from 'next/script';
 export default function GoogleTag() {
     return (
         <>
-            {/* Google Tag Manager / Analytics Placeholder */}
-            {/* Replace G-XXXXXXXXXX with actual Measurement ID */}
+            {/* Google Tag Manager - GTM-NS6QTKN */}
+            <Script id="google-tag-manager" strategy="afterInteractive">
+                {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NS6QTKN');
+                `}
+            </Script>
+
+            {/* Google Analytics 4 & Ads (Hybrid Implementation) */}
             <Script
-                src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+                src="https://www.googletagmanager.com/gtag/js?id=G-E7VSPDXLF3"
                 strategy="afterInteractive"
             />
             <Script id="google-analytics" strategy="afterInteractive">
                 {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
 
-          gtag('config', 'G-XXXXXXXXXX');
-        `}
+                  gtag('config', 'G-E7VSPDXLF3'); // GA4
+                  gtag('config', 'GT-TXX6TLW'); // Google Ads
+                `}
             </Script>
+
+            {/* Google Tag Manager (noscript) */}
+            <noscript>
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-NS6QTKN"
+                    height="0"
+                    width="0"
+                    style={{ display: 'none', visibility: 'hidden' }}
+                />
+            </noscript>
         </>
     );
 }
