07e8a015ab86d17d32b0730757befbffa8603800
Mobo4 <31019244+Mobo4@users.noreply.github.com>
2025-12-08 18:17:22 -0800

    Add Facebook Pixel (ID: 395306154557054)

--- components/TrackingScripts.tsx
+++ components/TrackingScripts.tsx
@@ -36,6 +36,32 @@ export default function TrackingScripts() {
         src="//cdn.callrail.com/companies/453122914/99c405dbdf8bc7511fbb/12/swap.js"
       />
 
+      {/* 
+        ------------------------------------------------------------
+        Meta Pixel (Facebook) 
+        ------------------------------------------------------------
+        ID: 395306154557054 (Found in analytics audit)
+      */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '395306154557054');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=395306154557054&ev=PageView&noscript=1"
+        />
+      </noscript>
+
     </>
   );
 }
